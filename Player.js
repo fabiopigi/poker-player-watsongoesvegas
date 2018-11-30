31ca69c6969d803554ba31e9343e5a653e627cb8
Fabio Pigagnelli <fabio.pigagnelli@quatico.com>
2018-11-30 13:12:58 +0100

    rais logic

--- Player.js
+++ Player.js
@@ -1,11 +1,35 @@
+const fetch = require('node-fetch');
+
 class Player {
   static get VERSION() {
     return '0.1';
   }
 
+
+
+
+
   static betRequest(gameState, bet) {
-    var betValue = gameState.current_buy_in - gameState.players[gameState.in_action]['bet'] + gameState.minimum_raise + 1;
-    bet(betValue);
+
+    const rankingUrl = "http://rainman.leanpoker.org/rank";
+    const cards = gameState.community_cards.concat(gameState.players[gameState.in_action].hole_cards);
+
+    let fetchRequest = fetch(rankingUrl, { method: 'GET', body: cards })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+
+        let betValue = gameState.current_buy_in - gameState.players[gameState.in_action]['bet']
+
+        let rank = json.rank;
+        if(rank > 1 ) {
+          betValue = betValue + gameState.minimum_raise + 1;
+        }
+
+        bet(betValue);
+      });
+
+
   }
 
   static showdown(gameState) {
